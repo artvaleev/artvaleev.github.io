e2309f4021723d1ccb5434a87de3528b26c91232
Artur Valeev <artur.valeev@akvelon.com>
2020-05-07 18:55:13 +0300

    + react demo

---
+++ charts/precache-manifest.06855478a863972ae204e6a6407dccda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2deca4e4ca78da387f8c4d3ef855aff1",
+    "url": "/charts/index.html"
+  },
+  {
+    "revision": "bb6396f28999bdf4de8d",
+    "url": "/charts/static/css/main.600f47df.chunk.css"
+  },
+  {
+    "revision": "c4961049d24820b322e5",
+    "url": "/charts/static/js/2.f16ba8c7.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/charts/static/js/2.f16ba8c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb6396f28999bdf4de8d",
+    "url": "/charts/static/js/main.d37652b1.chunk.js"
+  },
+  {
+    "revision": "91c2d53ed8e35dcead38",
+    "url": "/charts/static/js/runtime-main.dfeb1371.js"
+  }
+]);
